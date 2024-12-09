b836eb343ded35392d64a58072cb6ecfeb890d38
angus <angus1208@naver.com>
2024-12-09 16:49:38 +0900

    [feat] image 폴더에 .keep 파일 생성

--- src/main/java/com/metacoding/web_project/category/CategoryController.java
+++ src/main/java/com/metacoding/web_project/category/CategoryController.java
@@ -2,9 +2,11 @@ package com.metacoding.web_project.category;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class CategoryController {
     private final CategoryService categoryService;
+
 }
